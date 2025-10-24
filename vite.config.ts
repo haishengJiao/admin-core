5891be06ed13ff60d13245185f0e1e36b2b4cab9
v-jiaohaisheng <v-jiaohaisheng@360.cn>
2025-10-24 14:01:19 +0800

    chore: code-inspector-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,20 @@ import { fileURLToPath, URL } from 'node:url';
 
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { codeInspectorPlugin } from 'code-inspector-plugin';
 import { defineConfig } from 'vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    codeInspectorPlugin({
+      bundler: 'vite',
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
