7959387c606ad63c379248b73ec3fa46f2983ab4
v-jiaohaisheng <v-jiaohaisheng@360.cn>
2026-06-01 18:12:06 +0800

    feat: 偏好设置-小部件-主题快捷切换、语言快捷切换

---
+++ src/views/preferences/drawer-body/layout/Widget.vue
@@ -0,0 +1,11 @@
+<template>
+  <SwitchItem v-model="themeToggle">{{ $t('preferences.layout.widget.themeToggle') }}</SwitchItem>
+  <SwitchItem v-model="languageToggle">{{ $t('preferences.layout.widget.languageToggle') }}</SwitchItem>
+</template>
+
+<script lang="ts" setup>
+import SwitchItem from '../SwitchItem.vue';
+
+const themeToggle = defineModel<boolean>('themeToggle');
+const languageToggle = defineModel<boolean>('languageToggle');
+</script>
